94504a96e85afd923a267c5b6fd5c4df4a272965
Kevin Altman and Sam Umbach <pair@thinkrelevance.com>
2011-10-28 15:41:22 -0400

    Created database for profile. Created generic views for adding and viewing entries

---
+++ app/controllers/profiles_controller.rb
@@ -0,0 +1,18 @@
+class ProfilesController < ApplicationController
+  def new
+    @profile = Profile.new
+  end
+
+  def create
+    @profile = Profile.new params[:profile]
+    if @profile.save
+      redirect_to :action => :index
+    else
+      render :new
+    end
+  end
+
+  def index
+    @profiles = Profile.all
+  end
+end
